dd14e38f1db7abf1280783633f2dc9d336a46787
Abasshussein <abasshussein03@gmail.com>
2020-02-24 16:39:01 +0300

    Made the changes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-    $('#submit').click(function () {
-        var Name = $('#name').val();
-        var Email = $('#email').val();
-        var Message = $('#message');
-        var key = '43ab10333d46e19325f96d210ed40c49-us5';
-        if (Name == '' || Email == '' || Message == '') {
-            alert('Please make sure you have filled your credentials in the form correctly!');
-        } else {
-            alert(' Hello ' + Name + ' We have received your message. Thank you for messaging us. We try to be as responsive as possible. We will get back to you as soon as possible.');
-        }
-
-    });
-});
-$(document).ready(function () {
     $("#hussein").click(function () {
         $(this).hide();
         $(".abass").show();
