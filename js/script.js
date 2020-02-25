4581ea2029529f15fe9685955d4b27d8cdb053d6
Abasshussein <abasshussein03@gmail.com>
2020-02-25 08:33:46 +0300

    Updated the files to commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    $('#submit').click(function () {
+        var Name = $('#name').val();
+        var Email = $('#email').val();
+        var Message = $('#message');
+        if (Name == '' || Email == '' || Message == '') {
+            alert('Please make sure you have filled your credentials in the form correctly!');
+        } else {
+            alert(' Hello ' + Name + ' We have received your message. Thank you for messaging us. We try to be as responsive as possible. We will get back to you as soon as possible.');
+        }
+
+    });
+});
+
+$(document).ready(function () {
     $("#hussein").click(function () {
         $(this).hide();
         $(".abass").show();
